83779818f9acde5be91b2e97eca03674415ce7f3
Evgeniy Tonov <kejte1@mail.ru>
2025-03-12 18:54:38 +0300

    add allowed_hosts to environment

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'app']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['*'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
