9f46dded303a2ab8ded19f88f38e12d57a0dc300
Evgeniy Tonov <kejte1@mail.ru>
2025-04-27 12:49:38 +0300

    add filter to admin subscription

--- payments/admin.py
+++ payments/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from payments.models import Tariff, Subscription, Payments, Promocode, ActivatedPromocode
+from django.contrib.admin import SimpleListFilter
+from django.db.models import Case, When, BooleanField
+
+class SubscriptionFilter(SimpleListFilter):
+    title = 'С активной подпиской' # or use _('country') for translated title
+    parameter_name = 'С активной подпиской'
+
+    def lookups(self, request, model_admin):
+        return [('С активной подпиской','С активной подпиской')]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'С активной подпиской':
+            return queryset.annotate(active_sub=Case(
+                When(subscription_payments__is_actual=True, then=True),
+                default=False,
+                output_field=BooleanField()
+            )).filter(active_sub=True)
 
 class PromocodeInline(admin.TabularInline):
     model = Promocode
@@ -28,3 +45,4 @@ class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('profile',)
     search_fields = ('profile__username',)
     inlines = (PaymentsInline,)
+    list_filter = (SubscriptionFilter,)
